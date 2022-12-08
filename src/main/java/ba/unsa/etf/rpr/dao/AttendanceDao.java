7ce6198a4563a343c185efb3d9ac80000cffb154
Nerma <nkadric1@etf.unsa.ba>
2022-12-08 21:37:15 +0100

    added comment

--- src/main/java/ba/unsa/etf/rpr/dao/AttendanceDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AttendanceDao.java
@@ -6,7 +6,7 @@ import java.util.List;
 /**
  * @Kadrić Nerma
  * Interface for Attendance domain bean as Dao
-
+ * method getbyworkhours actually gives  to user list of employees by how much hours every of them spend at work
  */
 public interface AttendanceDao extends Dao<Attendance> {
     List<Attendance> getByWorkHours(int hours);
