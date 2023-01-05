16b7a25066efcf699df0dcedc5ed6948babdd47c
Nerma <nkadric1@etf.unsa.ba>
2023-01-05 21:49:09 +0100

    small changes in departmentcontroller

--- src/main/java/ba/unsa/etf/rpr/controllers/DepartmentController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/DepartmentController.java
@@ -4,6 +4,7 @@ import ba.unsa.etf.rpr.business.DepartmentManager;
 import ba.unsa.etf.rpr.domain.Departments;
 
 import ba.unsa.etf.rpr.exceptions.EmployeeException;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -16,10 +17,12 @@ public class DepartmentController {
     private DepartmentManager manager=new DepartmentManager();
     public ListView<Departments> departmentListView;
     public TextField depName;
+    public TextField hprice;
     private void refreshDepartment() throws EmployeeException{
         try{
             departmentListView.setItems(FXCollections.observableList(manager.getAll()));
         depName.setText("");
+        hprice.setText("");
         }catch(EmployeeException e){
             new Alert(Alert.AlertType.NONE,e.getMessage(), ButtonType.OK).show();
         }
@@ -29,27 +32,32 @@ public class DepartmentController {
         try{
             refreshDepartment();
             departmentListView.getSelectionModel().selectedItemProperty().addListener((obs,o,n)->{
-if(n!=null){ depName.setText(n.getDepname()); }
+if(n!=null){ depName.setText(n.getDepname());
+               hprice.setText(String.valueOf(n.getHourlypay()));}
             });
         } catch (EmployeeException e) {
           new Alert(Alert.AlertType.NONE, e.getMessage(),ButtonType.OK).show();
 
         }
     }
+    @FXML
     public void updateDep(ActionEvent actionEvent){
         try{
             Departments d=departmentListView.getSelectionModel().getSelectedItem();
             d.setDepname(depName.getText());
+            d.setHourlypay(Integer.parseInt(hprice.getText()));
             d=manager.update(d);
             refreshDepartment();
         }catch(EmployeeException e){
             new Alert(Alert.AlertType.NONE,e.getMessage(),ButtonType.OK).show();
         }
     }
+    @FXML
     public void addDep(ActionEvent actionEvent){
         try{
             Departments d=new Departments();
             d.setDepname(depName.getText());
+            d.setHourlypay(Integer.parseInt(hprice.getText()));
             d=manager.add(d);
             departmentListView.getItems().add(d);
             depName.setText("");
@@ -57,6 +65,7 @@ if(n!=null){ depName.setText(n.getDepname()); }
             new Alert(Alert.AlertType.NONE, e.getMessage(),ButtonType.OK).show();
         }
     }
+    @FXML
     public void deleteDep(ActionEvent actionEvent){
         try{
             Departments d=departmentListView.getSelectionModel().getSelectedItem();
@@ -65,5 +74,10 @@ if(n!=null){ depName.setText(n.getDepname()); }
         }catch (EmployeeException e){
             new Alert(Alert.AlertType.NONE, e.getMessage(),ButtonType.OK).show();
         }
+    }
+    @FXML
+    public void closeIt(ActionEvent actionEvent){
+        Platform.exit();
+        System.exit(0);
     }
 }
