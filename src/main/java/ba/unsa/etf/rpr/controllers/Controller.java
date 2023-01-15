bed5167d6ebf1ff2598afa740525119713cb38e0
Nerma <nkadric1@etf.unsa.ba>
2023-01-15 23:26:43 +0100

    added button color

--- src/main/java/ba/unsa/etf/rpr/controllers/Controller.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/Controller.java
@@ -4,16 +4,22 @@ import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.dao.EmployeeDAOSQLImpl;
 import ba.unsa.etf.rpr.dao.UsersDAOSQLImpl;
 import ba.unsa.etf.rpr.dao.UsersDao;
+import com.sun.javafx.fxml.builder.JavaFXSceneBuilder;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.stage.StageStyle;
+
 
 import java.io.IOException;
 import java.util.Objects;
@@ -25,15 +31,44 @@ public class Controller {
     public PasswordField passfield;
     public Button bBtn;
 
+//    @FXML
+//    private void onenter(KeyEvent keyEvent) throws IOException {
+//    if (keyEvent.getCode()==KeyCode.ENTER) {
+//        if (fieldUser.getText().isEmpty() ) {
+//            infobox("Please, enter username!", null, "Field is empty.");
+//        }
+//        else if ( passfield.getText().isEmpty()){
+//            infobox("Please, enter password!", null, "Field is empty.");
+//        }
+//        String us=fieldUser.getText();
+//        String pas=passfield.getText();
+//        UsersDAOSQLImpl u=new UsersDAOSQLImpl();
+//        boolean flag=u.validate(us,pas);
+//        if (!flag) {
+//            infobox("Please, enter correct username and password!", null, "Failed input!");
+//            fieldUser.clear();
+//            passfield.clear();
+//        }else {
+//
+//            openDialog("Company report!", "/FXML/new.fxml", new PageController());
+//        }
+//
+//    }
+//}
+    
+
     private void openDialog(String title, String file, Object controller) {
         try {
             FXMLLoader l = new FXMLLoader(getClass().getResource(file));
             l.setController(controller);
             Stage s = new Stage();
-            s.setScene(new Scene(l.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            Scene scene=new Scene(l.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
+            scene.setFill(Color.TRANSPARENT);
+       s.initStyle(StageStyle.TRANSPARENT);
+            s.setScene(scene);
             s.setTitle(title);
-            s.initStyle(StageStyle.UTILITY);
             s.setResizable(false);
+
             s.show();
         } catch (Exception e) {
             new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
@@ -56,7 +91,10 @@ String us=fieldUser.getText();
             fieldUser.clear();
             passfield.clear();
         }else {
+
             openDialog("Company report!", "/FXML/new.fxml", new PageController());
+
+
         }
 
 }
