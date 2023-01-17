6fe2c2b8555f639845ed10e562cf66e7488e0d3c
Nerma <nkadric1@etf.unsa.ba>
2023-01-17 12:17:23 +0100

    change of chart

--- src/main/java/ba/unsa/etf/rpr/controllers/StatisticofProController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/StatisticofProController.java
@@ -8,10 +8,7 @@ import ba.unsa.etf.rpr.exceptions.EmployeeException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
+import javafx.scene.chart.*;
 import javafx.scene.layout.Background;
 
 import java.util.ArrayList;
@@ -19,48 +16,20 @@ import java.util.Arrays;
 import java.util.List;
 
 public class StatisticofProController {
-
-private BarChart<String, Number> barChart;
-private CategoryAxis catAxis;
-private NumberAxis numAxis;
-private ObservableList<String> projectNames= FXCollections.observableArrayList();
-private ProjectDAOSQLImpl projectDAOSQL=new ProjectDAOSQLImpl();
+@FXML
+private LineChart<?, ?> Chart;
 private EmployeeDAOSQLImpl employeeDAOSQL=new EmployeeDAOSQLImpl();
- private XYChart.Series<String ,Integer> s=new XYChart.Series<>();
+
 @FXML
     private void initialize() throws EmployeeException {
+    XYChart.Series s=new XYChart.Series();
+    s.getData().add(new XYChart.Data<>("Project 1", employeeDAOSQL.returnNumberofEmployees(200)));
+    s.getData().add(new XYChart.Data<>("Project 2", employeeDAOSQL.returnNumberofEmployees(201)));
+    s.getData().add(new XYChart.Data<>("Project 3", employeeDAOSQL.returnNumberofEmployees(202)));
+    Chart.getData().addAll(s);
+    Chart.lookup(".chart-plot-background").setStyle("-fx-background-color: transparent;");
 
- catAxis=new CategoryAxis();
-    List<Project> plist=projectDAOSQL.getAll();
-    List<XYChart.Data<String,Number>> d=new ArrayList<>(plist.size());
 
-    for(Project x:plist){
-        projectNames.add(Arrays.asList(x.getProject_name()).toString());
-          } catAxis.setCategories(projectNames);
-    d.add(new XYChart.Data<>( catAxis.getAccessibleText(), 20));
-    XYChart.Series<String,Number> series=new XYChart.Series<>(FXCollections.observableList(d));
-   numAxis=new NumberAxis();
-
-    barChart=new BarChart<>(catAxis,numAxis);
-    barChart.setTitle("Statistics");
-
-    barChart.setLegendVisible(false);
-    barChart.setCategoryGap(0.0);
-    barChart.setBarGap(0.1);
-    barChart.setBackground(Background.EMPTY);
-    barChart.setVerticalGridLinesVisible(false);
-    barChart.getData().add(series);
 }
-public  void setProjectData(List<Employee> projectData){
-//    int[] counter=new int[10];
-//    for(Employee p: projectData){
-//        int i=p.getProject_id()-1;
-//        counter[i]++;
-//    }
-//
-//    for(int j=0;j<counter.length;j++){
-//        s.getData().add(new XYChart.Data<>(projectNames.get(j),counter[j]));
-//    }
-//    barChart.getData().add(s);
-}
+
 }
