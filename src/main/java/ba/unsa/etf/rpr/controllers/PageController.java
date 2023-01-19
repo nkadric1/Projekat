ec00b085c1da08fac971c0a0934009a2beb3057f
Nerma <nkadric1@etf.unsa.ba>
2023-01-19 19:01:47 +0100

    changed field to choicebox

--- src/main/java/ba/unsa/etf/rpr/controllers/PageController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/PageController.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.DepartmentManager;
 import ba.unsa.etf.rpr.business.EmployeeManager;
 import ba.unsa.etf.rpr.dao.*;
 import ba.unsa.etf.rpr.domain.Departments;
@@ -54,52 +55,59 @@ import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 public class PageController {
     public TextArea txtArea;
     private static final String filename = "ListOfEmployees.txt";
+
+    private EmployeeModel employeeModel = new EmployeeModel();
+
+
     public TextField empidfield;
     public TextField fnamefield;
     public TextField lnamefield;
     public TextField addressfield;
     public DatePicker hdatefield;
     public Button addempbttn;
-    public TextField depfield;
-    public  TextField proid;
+    public ChoiceBox<Departments> depfield;
+    public TextField proid;
     public TextField edufield;
     public TextField profield;
     public TextField salfield;
-    private EmployeeDAOSQLImpl employeeDAOSQL = new EmployeeDAOSQLImpl();
-    private DepartmentsDAOSQLImpl depSql = new DepartmentsDAOSQLImpl();
     private TilePane t = new TilePane();
     private List<Departments> depList;
     public TableView<Employee> emptab;
     public Button sEmp;
     public Button getEmp;
     public javafx.scene.image.Image img = new Image("IMAGES/add.png");
-    public Image img2=new Image("IMAGES/update.png");
-    public Image img3=new Image("IMAGES/delete.png");
+    public Image img2 = new Image("IMAGES/update.png");
+    public Image img3 = new Image("IMAGES/delete.png");
     public Button upempbttn;
     public Button delempbttn;
     public TableColumn<Employee, Integer> empIdcol;
-    public TableColumn<Employee, Integer> depemp;
+    public TableColumn<Employee, Departments> depemp;
     public TableColumn<Employee, String> empNamecol;
     public TableColumn<Employee, Date> emphdatecol;
-    public TableColumn<Employee,Integer> procolumn;
-    public TableColumn<Employee,Integer> salcol;
+    public TableColumn<Employee, Integer> procolumn;
+    public TableColumn<Employee, Integer> salcol;
     public MenuItem close;
-    private EmployeeManager manager=new EmployeeManager();
+    private EmployeeManager manager = new EmployeeManager();
+    private DepartmentManager departmentManager = new DepartmentManager();
+
     @FXML
     public void openED(ActionEvent actionEvent) {
         openDialog("Manage departments", "/FXML/dep.fxml", new DepartmentController());
     }
+
     @FXML
-    public void openEP(ActionEvent actionEvent){
+    public void openEP(ActionEvent actionEvent) {
         openDialog("Manage projects", "/FXML/pro.fxml", new projectController());
     }
+
     @FXML
-    public void getStatisticofProjects(ActionEvent actionEvent){
+    public void getStatisticofProjects(ActionEvent actionEvent) {
         openDialog("Statistics of projects", "/FXML/statistic.fxml", new StatisticofProController());
     }
+
     @FXML
-    public void OnAbout(ActionEvent actionEvent){
-        openDialog("About","/FXML/about.fxml",null);
+    public void OnAbout(ActionEvent actionEvent) {
+        openDialog("About", "/FXML/about.fxml", null);
     }
 
     private void openDialog(String title, String file, Object controller) {
@@ -117,11 +125,12 @@ public class PageController {
             e.printStackTrace();
         }
     }
-@FXML public void showall(ActionEvent actionEvent) throws EmployeeException {
- refreshEmployees();
-        List<Employee> emlist=employeeDAOSQL.getAll();
-        emptab.getItems().addAll(emlist);
-}
+
+    @FXML
+    public void showall(ActionEvent actionEvent) throws EmployeeException {
+        refreshEmployees();
+    }
+
     @FXML
     public void closeIt(ActionEvent actionEvent) {
         Platform.exit();
@@ -130,21 +139,21 @@ public class PageController {
 
     @FXML
     public void saveIt(ActionEvent actionEvent) throws IOException, EmployeeException {
-        Path path = Paths.get(filename);
+        /*Path path = Paths.get(filename);
         BufferedWriter bw = Files.newBufferedWriter(path);
         try {
             Iterator<Employee> iterator = employeeDAOSQL.getByHireDate().iterator();
             while (iterator.hasNext()) {
                 Employee temp = iterator.next();
-                bw.write(String.format("%s\t%s\t%s\t%s\t%s\t%s", temp.getFirst_name(), temp.getLast_name(), temp.getAddress(), temp.getHire_date(),temp.getEdu(),temp.getPayoff()));
+                bw.write(String.format("%s\t%s\t%s\t%s\t%s\t%s", temp.getFirst_name(), temp.getLast_name(), temp.getAddress(), temp.getHire_date(), temp.getEdu(), temp.getPayoff()));
                 bw.newLine();
             }
-    txtArea.setText("List of employees has been saved.");
+            txtArea.setText("List of employees has been saved.");
         } finally {
             if (bw != null) {
                 bw.close();
             }
-        }
+        }*/
     }
 
     @FXML
@@ -167,6 +176,11 @@ public class PageController {
 
     @FXML
     public void initialize() {
+        try {
+            depfield.setItems(FXCollections.observableList(departmentManager.getAll()));
+        } catch (EmployeeException e) {
+            throw new RuntimeException(e);
+        }
         txtArea.setBackground(Background.fill(Color.web("darkseagreen")));
         txtArea.setText("An insight into the administrative \n part of the company.");
         sEmp.setBackground(Background.fill(Color.web("darkseagreen")));
@@ -180,178 +194,122 @@ public class PageController {
         empIdcol.setCellValueFactory(new PropertyValueFactory<Employee, Integer>("id"));
         empNamecol.setCellValueFactory(new PropertyValueFactory<Employee, String>("First_name"));
         emphdatecol.setCellValueFactory(new PropertyValueFactory<Employee, Date>("Hire_date"));
-        depemp.setCellValueFactory(new PropertyValueFactory<Employee, Integer>("department_id"));
-       salcol.setCellValueFactory(new PropertyValueFactory<Employee,Integer>("payoff"));
-        procolumn.setCellValueFactory(new PropertyValueFactory<Employee,Integer>("project_id"));
+        depemp.setCellValueFactory(new PropertyValueFactory<Employee, Departments>("department"));
+        salcol.setCellValueFactory(new PropertyValueFactory<Employee, Integer>("payoff"));
+        procolumn.setCellValueFactory(new PropertyValueFactory<Employee, Integer>("project_id"));
+
+        fnamefield.textProperty().bindBidirectional(employeeModel.fname);
+        lnamefield.textProperty().bindBidirectional(employeeModel.lname);
+        addressfield.textProperty().bindBidirectional(employeeModel.address);
+        hdatefield.valueProperty().bindBidirectional(employeeModel.hdate);
+        depfield.valueProperty().bindBidirectional(employeeModel.did);
+        profield.textProperty().bindBidirectional(employeeModel.pid);
+        edufield.textProperty().bindBidirectional(employeeModel.edu);
+        salfield.textProperty().bindBidirectional(employeeModel.sal);
 
         emptab.getSelectionModel().selectedItemProperty().addListener((obs, oldEmployee, newEmployee) -> {
-            EmployeeModel employeeModel = new EmployeeModel();
-            Employee employee=newEmployee;
-            employeeModel.fromEmployee(employee);
-            fnamefield.textProperty().bindBidirectional(employeeModel.fname);
-            lnamefield.textProperty().bindBidirectional(employeeModel.lname);
-            addressfield.textProperty().bindBidirectional(employeeModel.address);
-            hdatefield.valueProperty().bindBidirectional(employeeModel.hdate);
-            depfield.textProperty().bindBidirectional(employeeModel.did);
-            profield.textProperty().bindBidirectional(employeeModel.pid);
-            edufield.textProperty().bindBidirectional(employeeModel.edu);
-            salfield.textProperty().bindBidirectional(employeeModel.sal);
+            if (newEmployee != null) {
+                employeeModel.fromEmployee(newEmployee);
+            }
         });
-        try {
-            emptab.setItems(FXCollections.observableList(employeeDAOSQL.getAll()));
-          refreshEmployees();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
+        refreshEmployees();
     }
 
     @FXML
-    public void addNew(ActionEvent actionEvent) throws EmployeeException {
-
-        try{
-            Employee e = new Employee();
-        e.setFirst_name(fnamefield.getText());
-        e.setLast_name(lnamefield.getText());
-        e.setAddress(addressfield.getText());
-        if (hdatefield.getValue() == null) {
-            e.setHire_date(LocalDate.now());
-        } else {
-            e.setHire_date(hdatefield.getValue());
-        }
-        e.setDepartment_id(Integer.parseInt(depfield.getText()));
-        e.setProject_id(Integer.parseInt(profield.getText()));
-        e.setEdu(edufield.getText());
-        e.setPayoff(Integer.parseInt(salfield.getText()));
-        e=manager.add(e);
-        emptab.getItems().add(e);
-        txtArea.setText("A new employee has been added.");
-        emptab.refresh();
-        fnamefield.setText("");
-        lnamefield.setText("");
-        addressfield.setText("");
-        hdatefield.setValue(null);
-        depfield.setText("");
-        profield.setText("");
-        edufield.setText("");
-        salfield.setText("");
-        }catch (EmployeeException e)
-        {
+    public void addNew(ActionEvent actionEvent) {
+        try {
+            manager.add(employeeModel.toEmployee());
+            refreshEmployees();
+        } catch (EmployeeException e) {
             new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
         }
     }
-    //ispraviti metodu za update
-    @FXML
-    public void UpdateEmp(ActionEvent actionEvent) throws  EmployeeException{
-try{
-    Employee e= emptab.getSelectionModel().getSelectedItem();
-    emptab.getSelectionModel().select(null);
-    e.setFirst_name(fnamefield.getText());
-    e.setLast_name(lnamefield.getText());
-    e.setAddress(addressfield.getText());
-    if (hdatefield.getValue() == null) {
-        e.setHire_date(LocalDate.now());
-    } else {
-        e.setHire_date(hdatefield.getValue());
-    }
-    try{
-    DepartmentsDAOSQLImpl d=new DepartmentsDAOSQLImpl();
-    Departments dep=d.ReturnDepartmentForId(Integer.parseInt(depfield.getText()));
-        e.setDepartment_id(Integer.parseInt(depfield.getText()));
 
-            e.setProject_id(Integer.parseInt(profield.getText()));
-            e.setEdu(edufield.getText());
-            e.setPayoff(Integer.parseInt(salfield.getText()));
-            e=manager.update(e);
-            txtArea.setText("Employee " +e.getFirst_name()+" "+ e.getLast_name() +"\n  has been updated.");
+    @FXML
+    public void UpdateEmp(ActionEvent actionEvent) {
+        try {
+            Employee e = employeeModel.toEmployee();
+            manager.update(e);
             refreshEmployees();
-    }
-   catch (Exception d){
-        new Alert(Alert.AlertType.NONE,"Department ID is not valid.",ButtonType.OK).show();;
-   }
-} catch (Exception e) {
-    throw new RuntimeException(e);
-}
+        } catch (Exception d) {
+            d.printStackTrace();
+            new Alert(Alert.AlertType.NONE, d.getMessage(), ButtonType.OK).show();
+        }
     }
 
     @FXML
-    public void searchEmp(ActionEvent actionEvent) throws EmployeeException {
-     try   {
-            Employee e = employeeDAOSQL.getfromID(Integer.parseInt(empidfield.getText()));
+    public void searchEmp(ActionEvent actionEvent) {
+        try {
+            Employee e = manager.getById(Integer.parseInt(empidfield.getText()));
             ObservableList<Employee> emp = FXCollections.observableArrayList();
             emp.add(e);
-         txtArea.setText("Search has been successful!");
-
-         emptab.setItems(emp);
-        }catch(EmployeeException e){
-         txtArea.setText("Employee with ID: " + Integer.parseInt(empidfield.getText())+ "\n is not available.");
-     }
+            txtArea.setText("Search has been successful!");
+            emptab.setItems(emp);
+        } catch (EmployeeException e) {
+            txtArea.setText("Employee with ID: " + Integer.parseInt(empidfield.getText()) + "\n is not available.");
+        }
     }
 
 
-@FXML
-public void DeleteEmp(ActionEvent actionEvent) throws EmployeeException{
-    try {
-        Employee ee= (Employee) emptab.getSelectionModel().getSelectedItem();
-        manager.delete(ee.getId());
-        txtArea.setText("Employee " +ee.getFirst_name()+ " " + ee.getLast_name() +"\n has been deleted.");
-        emptab.getItems().remove(ee);
-    }catch (EmployeeException e){
-        new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
+    @FXML
+    public void DeleteEmp(ActionEvent actionEvent) {
+        try {
+            Employee ee = (Employee) emptab.getSelectionModel().getSelectedItem();
+            manager.delete(ee.getId());
+            txtArea.setText("Employee " + ee.getFirst_name() + " " + ee.getLast_name() + "\n has been deleted.");
+            emptab.getItems().remove(ee);
+        } catch (EmployeeException e) {
+            new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
+        }
     }
-}
-@FXML
-  public void getListofemp(ActionEvent actionEvent) throws EmployeeException{
-   try {
-        List<Employee> e = employeeDAOSQL.searchByProject(Integer.parseInt(proid.getText()));
-        ObservableList<Employee> emp = FXCollections.observableArrayList();
-        emp.addAll(e);
-       emptab.setItems(emp);
 
-   }
-     catch (EmployeeException e){
-       txtArea.setText("Project with ID "+ Integer.parseInt(proid.getText())+ "\n is not available.");
-     }
-  }
-
-    public PageController(EmployeeDAOSQLImpl employeeDAOSQL) {
-        this.employeeDAOSQL = employeeDAOSQL;
+    @FXML
+    public void getListofemp(ActionEvent actionEvent) throws EmployeeException {
+        try {
+            emptab.setItems(FXCollections.observableList(manager.getByProject(Integer.parseInt(proid.getText()))));
+        } catch (EmployeeException e) {
+            txtArea.setText("Project with ID " + Integer.parseInt(proid.getText()) + "\n is not available.");
+        }
     }
-    public PageController(){}
-    private void refreshEmployees() throws EmployeeException{
-        try{
+
+    private void refreshEmployees() {
+        try {
             emptab.setItems(FXCollections.observableList(manager.getAll()));
             fnamefield.setText("");
             lnamefield.setText("");
             addressfield.setText("");
             hdatefield.setValue(null);
-            depfield.setText("");
+            depfield.setValue(null);
             profield.setText("");
             edufield.setText("");
             salfield.setText("");
-        }catch (EmployeeException e){
+        } catch (EmployeeException e) {
             new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
         }
     }
 
 
     public class EmployeeModel {
+
+        public SimpleIntegerProperty id = new SimpleIntegerProperty();
         public SimpleStringProperty fname = new SimpleStringProperty("");
         public SimpleStringProperty lname = new SimpleStringProperty("");
         public SimpleStringProperty address = new SimpleStringProperty("");
         public SimpleObjectProperty<LocalDate> hdate = new SimpleObjectProperty<LocalDate>();
-        public SimpleStringProperty did = new SimpleStringProperty("");
-        public SimpleStringProperty pid=new SimpleStringProperty("");
+        public SimpleObjectProperty<Departments> did = new SimpleObjectProperty();
+        public SimpleStringProperty pid = new SimpleStringProperty("");
         public SimpleStringProperty edu = new SimpleStringProperty("");
-        public SimpleStringProperty sal=new SimpleStringProperty("");
+        public SimpleStringProperty sal = new SimpleStringProperty("");
 
 
         public void fromEmployee(Employee e) {
+            this.id.set(e.getId());
             this.fname.set(e.getFirst_name());
             this.lname.set(e.getLast_name());
             this.address.set(e.getAddress());
             this.hdate.set(e.getHire_date());
-            this.did.set(String.valueOf(e.getDepartment_id()));
+            this.did.set(e.getDepartment());
             this.pid.set(String.valueOf(e.getProject_id()));
             this.edu.set(e.getEdu());
             this.sal.set(String.valueOf(e.getPayoff()));
@@ -360,13 +318,15 @@ public void DeleteEmp(ActionEvent actionEvent) throws EmployeeException{
 
         public Employee toEmployee() {
             Employee e = new Employee();
-            e.setFirst_name(this.fname.getName());
-            e.setLast_name(this.lname.getName());
-            e.setAddress(this.address.getName());
-            e.setDepartment_id(Integer.parseInt(this.did.getValue()));
+            e.setId(this.id.getValue());
+            e.setFirst_name(this.fname.getValue());
+            e.setLast_name(this.lname.getValue());
+            e.setAddress(this.address.getValue());
+            // e.setDepartment_id(this.did.getValue().getId());
+            e.setDepartment(this.did.getValue());
             e.setProject_id(Integer.parseInt(this.pid.getValue()));
             e.setHire_date(this.hdate.getValue());
-            e.setEdu(this.edu.getName());
+            e.setEdu(this.edu.getValue());
             e.setPayoff(Integer.parseInt(this.sal.getValue()));
             return e;
         }
