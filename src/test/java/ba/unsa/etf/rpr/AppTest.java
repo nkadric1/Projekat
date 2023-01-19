e8ae266b50c3c6bff388067b2908e7f9b6d9d1b9
Nerma <nkadric1@etf.unsa.ba>
2023-01-19 13:09:01 +0100

    change in apptest

--- src/test/java/ba/unsa/etf/rpr/AppTest.java
+++ src/test/java/ba/unsa/etf/rpr/AppTest.java
@@ -1,8 +1,10 @@
 package ba.unsa.etf.rpr;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.Test;
+
 
 /**
  * Unit test for simple App.
@@ -15,6 +17,7 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+
+        //assertTrue( true );
     }
 }
