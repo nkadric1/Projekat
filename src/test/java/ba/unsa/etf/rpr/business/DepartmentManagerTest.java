b66b1163a5f71e1c851fb7b75849c6cb19632e15
Nerma <nkadric1@etf.unsa.ba>
2023-01-24 10:17:00 +0100

    new tests

--- src/test/java/ba/unsa/etf/rpr/business/DepartmentManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/business/DepartmentManagerTest.java
@@ -2,10 +2,7 @@ package ba.unsa.etf.rpr.business;
 
 import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.dao.DepartmentsDAOSQLImpl;
-import ba.unsa.etf.rpr.dao.EmployeeDAOSQLImpl;
 import ba.unsa.etf.rpr.domain.Departments;
-import ba.unsa.etf.rpr.domain.Employee;
-import ba.unsa.etf.rpr.domain.Project;
 import ba.unsa.etf.rpr.exceptions.EmployeeException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -21,46 +17,58 @@ import java.util.List;
 import static org.mockito.Mockito.when;
 
 public class DepartmentManagerTest {
-    private  DepartmentManager departmentManager;
+    private DepartmentManager departmentManager;
     private Departments d;
     private DepartmentsDAOSQLImpl departmentsDAOSQL;
     private List<Departments> departments;
+
     @BeforeEach
-    public void initializeObj(){
-       departmentManager= new DepartmentManager();
+    public void initializeObj() {
+        departmentManager = new DepartmentManager();
         Mockito.mock(DepartmentManager.class);
-        d=new Departments();
+        d = new Departments();
         d.setDepname("Backend-developer");
-      d.setHourlypay(15);
-    departmentsDAOSQL=Mockito.mock(DepartmentsDAOSQLImpl.class);
-        departments=new ArrayList<>();
-     departments.addAll(Arrays.asList(new Departments(51,"Marketing",10),
-                new Departments(52,"Software archi",20)));
+        d.setHourlypay(15);
+        departmentsDAOSQL = Mockito.mock(DepartmentsDAOSQLImpl.class);
+        departments = new ArrayList<>();
+        departments.addAll(Arrays.asList(new Departments(51, "Marketing", 10),
+                new Departments(52, "Software archi", 20)));
 
     }
-    @Test
-    void addNewDepTest() throws EmployeeException{
-        MockedStatic<DaoFactory> daoFactoryMockedStatic=Mockito.mockStatic(DaoFactory.class);
 
- DepartmentsDAOSQLImpl depDao=Mockito.mock(DepartmentsDAOSQLImpl.class);
+    @Test
+    void addNewDepTest() throws EmployeeException {
+        MockedStatic<DaoFactory> daoFactoryMockedStatic = Mockito.mockStatic(DaoFactory.class);
+
+        DepartmentsDAOSQLImpl depDao = Mockito.mock(DepartmentsDAOSQLImpl.class);
         daoFactoryMockedStatic.when(DaoFactory::departmentDao).thenReturn(depDao);
 
         when(depDao.add(d)).thenReturn(d);
 
-       departmentManager.add(d);
+        departmentManager.add(d);
         Assertions.assertTrue(true);
     }
-    @Test
-    void UpdateDepTest() throws EmployeeException{
-        d.setHourlypay(23);
-        MockedStatic<DaoFactory> daoFactoryMockedStatic=Mockito.mockStatic(DaoFactory.class);
 
-        DepartmentsDAOSQLImpl depDao=Mockito.mock(DepartmentsDAOSQLImpl.class);
+    @Test
+    void UpdateDepTest() throws EmployeeException {
+        d.setHourlypay(23);
+        MockedStatic<DaoFactory> daoFactoryMockedStatic = Mockito.mockStatic(DaoFactory.class);
+
+        DepartmentsDAOSQLImpl depDao = Mockito.mock(DepartmentsDAOSQLImpl.class);
         daoFactoryMockedStatic.when(DaoFactory::departmentDao).thenReturn(depDao);
 
         when(depDao.update(d)).thenReturn(d);
 
         departmentManager.update(d);
         Assertions.assertTrue(true);
+    }
+
+    @Test
+    void addDep()throws EmployeeException {
+      Departments newDep = new Departments(2, "GameDeveloper", 25);
+      newDep.setId(0);
+      departmentManager.add(newDep);
+        Assertions.assertTrue(true);
+        Mockito.verify(departmentManager).add(newDep);
     }
 }
